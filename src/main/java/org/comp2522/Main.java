d825f38352dacd44e868868ece448f65d9a180d1
Caroline Lin <caroline.yclin@gmail.com>
2022-09-05 22:15:26 -0700

    Change the print message to my favourite animal with an adj.

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -2,6 +2,6 @@ package org.comp2522;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The Giant Phantom Jelly is majestic!");
+        System.out.println("The Giant Phantom Jelly is truly majestic!");
     }
 }
