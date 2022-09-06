77e4ba6c4c31ae962319d8723f1a8af2dfc9fe45
Caroline Lin <caroline.yclin@gmail.com>
2022-09-06 11:20:28 -0700

    Changed the indentation from 4 to 2.

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,10 @@
 package org.comp2522;
 
+/**
+ * This is a program that prints my favourite animal.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("The Giant Phantom Jelly is truly majestic!");
-    }
+  public static void main(String[] args) {
+    System.out.println("The Giant Phantom Jelly is truly majestic!");
+  }
 }
